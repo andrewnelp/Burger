fd1797e8dbdcdcb7527500b238411af72c781310
Andrey Nelepov <nelepow@gmail.com>
2019-06-16 11:22:01 -0400

    added models

--- models/burger.js
+++ models/burger.js
@@ -1,25 +1,25 @@
 let orm = require("../config/orm.js");
 let burger = {
-  selectAll: function (cb) {
-    orm.selectAll("burgers", (res) => {
+  selectAll: cb => {
+    orm.selectAll("burgers", res => {
       cb(res);
     });
   },
 
-  insertOne: function (cols, vals, cb) {
+  insertOne: (cols, vals, cb) => {
     orm.insertOne("burgers", cols, vals, (res) => {
       cb(res);
     });
   },
 
-  updateOne: function (objColVals, condition, cb) {
-    orm.updateOne("burgers", objColVals, condition, (res) => {
+  updateOne: (objColVals, condition, cb) => {
+    orm.updateOne("burgers", objColVals, condition, res => {
       cb(res);
     });
   },
 
-  deleteOne: function (condition, cb) {
-    orm.deleteOne("burgers", condition, (res) => {
+  deleteOne: (condition, cb) => {
+    orm.deleteOne("burgers", condition, res => {
       cb(res);
     });
   }
